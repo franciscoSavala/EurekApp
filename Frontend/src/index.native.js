e140d3d39ae53feef1a9b86900507952f9c7cfa5
Francisco Savala <franciscosavala01@gmail.com>
2024-06-09 23:20:09 -0300

    stash

--- Frontend/src/index.native.js
+++ Frontend/src/index.native.js
@@ -1,5 +1,5 @@
 import {AppRegistry} from 'react-native';
-import App from '../App';
+import App from './src/App';
 import {expo as appName} from 'Frontend/app.json';
 
 AppRegistry.registerComponent(appName.name, () => App);
