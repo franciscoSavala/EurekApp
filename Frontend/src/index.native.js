9b6afc7483138da67ff485c7ef6cb704f576c751
Francisco Savala <franciscosavala01@gmail.com>
2024-06-21 15:14:58 -0300

    Fixed broken frontend x2

--- Frontend/src/index.native.js
+++
@@ -1,5 +0,0 @@
-import {AppRegistry} from 'react-native';
-import App from './src/App';
-import {expo as appName} from 'Frontend/app.json';
-
-AppRegistry.registerComponent(appName.name, () => App);
