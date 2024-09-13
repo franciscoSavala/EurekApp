69a05174f1ad9ffa730e90c3bc6d6e5ee834ce96
nelsonramirocampos <nelsonramirocampos@gmail.com>
2024-09-13 17:42:06 -0300

    Se agrega List con los detalles de errores

--- Backend/src/main/java/com/eurekapp/backend/exception/ApiError.java
+++ Backend/src/main/java/com/eurekapp/backend/exception/ApiError.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -13,4 +15,5 @@ public class ApiError {
     private String error;
     private String message;
     private Integer status;
+    private List<String> details;  // Para almacenar mensajes adicionales, si es necesario.
 }
