43a16854ec670dcfb990ab0979913316669584b7
nelsonramirocampos <nelsonramirocampos@gmail.com>
2024-09-13 17:43:46 -0300

    Se mejora el manejo de errores y se agrega el listado de errores.

--- Backend/src/main/java/com/eurekapp/backend/exception/Handler.java
+++ Backend/src/main/java/com/eurekapp/backend/exception/Handler.java
@@ -12,7 +12,6 @@ import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 @ControllerAdvice
 public class Handler {
