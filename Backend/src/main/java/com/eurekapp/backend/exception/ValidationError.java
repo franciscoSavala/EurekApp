cc3372485fbe9c6ca22bf12cbeb100e4030ee23f
nelsonramirocampos <nelsonramirocampos@gmail.com>
2024-09-13 17:42:45 -0300

    Se agregan posibles errores.

--- Backend/src/main/java/com/eurekapp/backend/exception/ValidationError.java
+++ Backend/src/main/java/com/eurekapp/backend/exception/ValidationError.java
@@ -4,7 +4,14 @@ import lombok.Getter;
 
 @Getter
 public enum ValidationError {
+
+    INVALID_EMAIL("invalid_email", "El correo no puede estar vacío."),
+    INVALID_PASSWORD("invalid_password", "La contraseña no puede estar vacía."),
+    INVALID_CREDENTIALS("invalid_credentials", "Usuario y/o contraseña incorrecta."),
+    USER_NOT_FOUND("user_not_found", "No se encontró el usuario con el username %s"),
+    REPEATED_EMAIL("repeated_email", "Ya existe un usuario con ese correo"),
     FOUND_DATE_ERROR("found_date_error", "The date must be before system date");
+
     private final String code;
     private final String error;
 
