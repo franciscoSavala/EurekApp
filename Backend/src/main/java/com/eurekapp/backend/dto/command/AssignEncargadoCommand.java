d53f696453a2c16f67f7fbb202b2aa3059e8e677
nelsonramirocampos <nelsonramirocampos@gmail.com>
2026-03-25 15:28:40 -0300

    EU-44

---
+++ Backend/src/main/java/com/eurekapp/backend/dto/command/AssignEncargadoCommand.java
@@ -0,0 +1,16 @@
+package com.eurekapp.backend.dto.command;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AssignEncargadoCommand {
+    @JsonProperty("userId")
+    private Long userId;
+}
